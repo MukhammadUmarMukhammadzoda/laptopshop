dfb02b852a1d39bf92fa58a881ac39ded3a92aae
unknown <mukhammadzodamukhammadumar@gmail.com>
2022-03-08 20:40:30 +0500

    Commit2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn noutuz.wsgi --log-file -
--- noutUz/settings.py
+++ noutUz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tsge&f3j87m8w(c*gw)0x7q&ra9rl0s4wgmw#+g#n601)7%0-w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 import os
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
