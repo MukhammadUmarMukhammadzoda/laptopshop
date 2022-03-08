a3a9bf900ca7b012fe284d8d90db961b976e811e
unknown <mukhammadzodamukhammadumar@gmail.com>
2022-03-08 20:59:57 +0500

    Commit7

--- noutUz/settings.py
+++ noutUz/settings.py
@@ -133,3 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
